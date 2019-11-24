39c00f8dfa7063ed33e59a5e2b667f496409e274
hmarf <hmarfUni@gmail.com>
2019-11-25 01:46:21 +0900

    create trunks.rb

---
+++ trunks.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Trunks < Formula
+  desc "Trunks is a simple command line tool for HTTP load testing."
+  homepage "https://github.com/hmarf/trunks"
+  url "https://github.com/hmarf/trunks/releases/download/v1.0/main"
+  version "1.0"
+  sha256 "2efc09e4b8a3e743db045be2ffba8268e37ef306ca3ba0ca4abd76af4d779e46"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install "trunks"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test trunks`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
