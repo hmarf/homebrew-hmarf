c28e5330d3831355147cf27ed03d80867a0e6080
hmarf <hmarfUni@gmail.com>
2021-04-15 21:41:49 +0900

    change ctest.rb

--- ctest.rb
+++ ctest.rb
@@ -2,30 +2,15 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ctest < Formula
-    desc ""
-    homepage ""
-    url "https://github.com/hmarf/cTest/archive/refs/tags/v0.0.1.tar.gz"
+    desc "Give color to the output according to the test result"
+    url "https://github.com/hmarf/cTest/releases/download/v0.0.1/ctest"
     sha256 "ea581dc48c7454f0bd036a2bde36988b38091b0d4e1253fe05b8245e438e7c26"
-  
-    # depends_on "cmake" => :build
-  
+
     def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
       bin.install "ctest"
-      # system "cmake", ".", *std_cmake_args
     end
   
     test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test gpixart`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
       system "true"
     end
   end
