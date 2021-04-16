89818f04582f25acd08a56ff50ede05fa1425b35
hmarf <hmarfUni@gmail.com>
2021-04-16 23:12:09 +0900

    chnage rb

--- ctest.rb
+++ ctest.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ctest < Formula
     desc "Give color to the output according to the test result"
-    url "https://github.com/hmarf/cTest/archive/refs/tags/v1.0.0.tar.gz"
+    url "https://github.com/hmarf/cTest/releases/download/v1.0.0/ctest"
     sha256 "bc367391ee17e080bb207aeb39f7a672092806babc8ddbf145831b5c951d21da"
 
     def install
