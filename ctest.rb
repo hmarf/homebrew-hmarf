61792dce6f4df0bb22233a6f34bbfc725ee8a914
hmarf <46663023+hmarf@users.noreply.github.com>
2021-04-16 23:27:53 +0900

    Update ctest.rb

--- ctest.rb
+++ ctest.rb
@@ -2,6 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ctest < Formula
+    version "v0.1.0"
     desc "Give color to the output according to the test result"
     url "https://github.com/hmarf/cTest/releases/download/v1.0.0/ctest"
     sha256 "bc367391ee17e080bb207aeb39f7a672092806babc8ddbf145831b5c951d21da"
