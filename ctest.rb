4c54d878c7c51da0c80a3277d3fd91dd2ce36231
hmarf <hmarfUni@gmail.com>
2021-04-16 22:35:57 +0900

    change ctest.rb

--- ctest.rb
+++ ctest.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ctest < Formula
     desc "Give color to the output according to the test result"
-    url "https://github.com/hmarf/cTest/releases/download/v0.0.1/ctest"
-    sha256 "c0b0b37eb0fbedb1a7ace04f7030e7fadbc3d90718d4e64899771072c11757f0"
+    url "https://github.com/hmarf/cTest/releases/download/v1.0.0/ctest"
+    sha256 "bc367391ee17e080bb207aeb39f7a672092806babc8ddbf145831b5c951d21da"
 
     def install
       bin.install "ctest"
